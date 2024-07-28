b6a5b024bbb7913c4c7dca09f6c4fb81b9e50cdb
Rolypoly74800 <lphalla.pl@gmail.com>
2024-07-28 12:05:04 +1000

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,86 +1,77 @@
 ECHO OFF
-set DOTNET_CLI_TELEMETRY_OPTOUT=1
-:MainMenu
 
+set pwd=%cd%
+
+:MainMenu
 echo Input Options:
 echo.
-echo ::1) Mount Storage (Series Console)
-echo ::2) Mount Storage (One Console)
-echo ::3) Get Temp XVD Owner
-echo ::4) Backup XBFS (Important file)
-echo ::5) Allow Emulators (Only needs to be run once, gives 1 error but seems to work)
-echo ::6) Backup Licences to USB:\Licenses
-echo ::7) Backup Saves to USB (Mount Storage First)
+echo ::1) Mount Storage
+echo ::2) Get Temp XVD Owner
+echo ::3) Backup XBFS (Important file)
+echo ::4) Allow Emulators (Only needs to be run once)
+echo ::5) Backup Licences to USB:\Licenses
+echo ::6) Backup Saves to USB (Mount Storage First)
 echo ::Q) Quit
 echo.
-echo Thanks to carrot-c4k3, xbox one research project team, tuxuser, lllsondowlll, Helloyunho, burninrubber0 and everyone else on the Xbox-Scene Discord
+echo Thanks to carrot-c4k3, xbox one research project team, tuxuser, lllsondowlll, burninrubber0, Helloyunho and everyone else on the Xbox-Scene Discord
 echo.
 set /p input=Input:
-if "%input%" == "1" call :MountStorageseries
-if "%input%" == "2" call :MountStorageone
-if "%input%" == "3" call :Tempxvd
-if "%input%" == "4" call :xbfsbackup
-if "%input%" == "5" call :allowemu
-if "%input%" == "6" call :sclip
-if "%input%" == "7" call :Dumpsaves
+if "%input%" == "1" call :MountStorage
+if "%input%" == "2" call :Tempxvd
+if "%input%" == "3" call :xbfsbackup
+if "%input%" == "4" call :allowemu
+if "%input%" == "5" call :sclip
+if "%input%" == "6" call :Dumpsaves
 if "%input%" == "Q" exit /b 0
 if "%input%" == "q" exit /b 0
+goto :MainMenu
 
-:MountStorageseries
+:MountStorage
 ECHO Mount Storage on Xbox Series
 echo.
-d:\dotnet\dotnet.exe msbuild d:\msbuild_tasks\mount_connectedstoragess.xml
+%pwd%\dotnet\dotnet.exe msbuild %pwd%\msbuild_tasks\mount_connectedstorage.xml
 echo.
-set /p input=Please type the harddisk number only you see above in the field Device\Harddisk##\Partition1=
+set /p input=Please type the harddrive number only you see above in the field Device\Harddisk##\Partition1=
 mklink /j T:\connectedStorage "\\?\GLOBALROOT\Device\Harddisk%input%\Partition1\"
-GOTO :mainmenu
-
-:MountStorageone
-ECHO Mount Storage on Xbox One
-echo.
-d:\dotnet\dotnet.exe msbuild d:\msbuild_tasks\mount_connectedstorage.xml
-echo.
-set /p input=Please type the harddisk number only you see above in the field Device\Harddisk##\Partition1=
-mklink /j T:\connectedStorage "\\?\GLOBALROOT\Device\Harddisk%input%\Partition1\"
-GOTO :mainmenu
+GOTO End
 
 :Tempxvd
 ECHO Get Temp XVD Owner
 echo.
-d:\dotnet\dotnet.exe msbuild d:\msbuild_tasks\get_tempxvd_owners.xml
-GOTO :mainmenu
+%pwd%\dotnet\dotnet.exe msbuild %pwd%\msbuild_tasks\get_tempxvd_owners.xml
+GOTO End
 
 :xbfsbackup
 ECHO Perform XBFS Backup
 echo.
-d:\dotnet\dotnet.exe msbuild d:\msbuild_tasks\xbfs_backup.xml
-GOTO :mainmenu
+%pwd%\dotnet\dotnet.exe msbuild %pwd%\msbuild_tasks\xbfs_backup.xml
+GOTO End
 
 :allowemu
 ECHO Allow Emulators (unsure if it works properly)
 echo.
-d:\dotnet\dotnet.exe msbuild d:\msbuild_tasks\allow_emulators.xml
-GOTO :mainmenu
+%pwd%\dotnet\dotnet.exe msbuild %pwd%\msbuild_tasks\allow_emulators.xml
+GOTO End
 
 :sclip
 ECHO Backup Licenses
-IF NOT EXIST D:\Licenses (
-    MKDIR D:\Licenses
+IF NOT EXIST %pwd%\Licenses (
+    MKDIR %pwd%\Licenses
 )
-copy s:\clip\*.* D:\Licenses
-GOTO :mainmenu
+copy s:\clip\*.* %pwd%\Licenses
+GOTO End
 
 :DumpSaves
 ECHO Dump Saves (Credit to burninrubber0 on Discord)
 echo.
-cd /d t:\connectedstorage
+cd /d t:connectedstorage
 for /R /D %%d in (.\*) do (
-    mkdir D:\xb1\saves%%~pnxd
+    mkdir %pwd%\xb1\saves%%~pnxd
 )
 for /R %%f in (.\*) do (
-    copy %%f D:\xb1\saves%%~pnxf
+    copy %%f %pwd%\xb1\saves%%~pnxf
 )
 d:
-GOTO :mainmenu
+GOTO End
 
 :End
